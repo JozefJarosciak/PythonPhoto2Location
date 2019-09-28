44be832efb0115ac0db64b6a963da179d5c43129
jozef_jarosciak <jarosciak@gmail.com>
2019-09-27 20:18:12 -0400

    further improvements

--- PythonPhoto2Location.py
+++ PythonPhoto2Location.py
@@ -1,27 +1,57 @@
+import calendar
+import datetime, glob, os, os.path
 import tkinter
-import glob
+import webbrowser
+from decimal import Decimal
+from threading import Thread
+from tkinter import *
+from tkinter import filedialog
+from typing import Optional, Any
+import country_converter as coco
+import pandas as pd
+import reverse_geocoder as rg
+from PIL import Image
 from PIL.ExifTags import GPSTAGS
 from PIL.ExifTags import TAGS
-from PIL import Image
-import reverse_geocoder as rg
-from pprint import pprint
-import datetime
+from gmplot import gmplot
+
+# ADD YOUR OWN GOOGLE MAPS API KEY, THE ONE BELOW WON'T WORK
+google_api_key = "IzaSyD2KMkrfQkzqNBEo-5iZDhDOlbvvDrO0dM"
 
 # Initialize the main window
 window = tkinter.Tk()
-# Size windows to 400 x 400px
-window.minsize(400, 400)
-# Give your window a title
+# Size window
+window.minsize(500, 500)
+# Title
 window.title("Photo To Location")
 # Set Icon
 window.wm_iconbitmap("window_icon.ico")
 
-# Place labels
-label = tkinter.Label(window, text="Coordinates:")
-label.place(x=10, y=10)
+entryText = tkinter.StringVar()
+textbox = tkinter.Entry(window, width=75, textvariable=entryText)
+textbox.place(x=10, y=20)
 
-label2 = tkinter.Label(window, text="Location:")
-label2.place(x=10, y=30)
+# Place labels
+link2 = Label(window, text="", fg="blue", cursor="hand2")
+link2.place(x=160, y=90)
+
+link1 = Label(window, text="", fg="blue", cursor="hand2")
+link1.place(x=270, y=90)
+
+label = tkinter.Label(window, text="")
+label.place(x=10, y=80)
+
+label2 = tkinter.Label(window, text="")
+label2.place(x=10, y=96)
+
+status_message = StringVar()
+status = Label(window, textvariable=status_message, bd=1, relief=SUNKEN, anchor=W)
+status.pack(side=BOTTOM, fill=X)
+
+text = Text(window, height=22, width=59)
+text.place(x=10, y=120)
+
+cpt = 0
 
 
 def get_labeled_exif(exif):
@@ -32,7 +62,7 @@ def get_labeled_exif(exif):
 
 
 def get_exif(filename):
-    image = Image.open(filename)
+    image: Optional[Any] = Image.open(filename)
     image.verify()
     return image._getexif()
 
@@ -69,8 +99,8 @@ def get_coordinates(geotags):
     return lat, lon
 
 
-def convert(date_time):
-    format = '%Y:%m:%d' # The format
+def converter(date_time):
+    format = '%Y:%m:%d'  # The format
     datetime_str = datetime.datetime.strptime(date_time, format)
     return datetime_str
 
@@ -92,53 +122,207 @@ def center(win):
 # Center Window on Screen
 center(window)
 
-# Initialize counter
-counter = 0
+
+def ask_quit():
+    window.destroy()
+    exit()
+
+
+def on_closing():
+    root = Tk()
+    root.destroy()
+    window.destroy()
+    exit()
+
+
+def press():
+    global cpt
+    root = Tk()
+    root.withdraw()
+    folder_selected = filedialog.askdirectory()
+    entryText.set(folder_selected)
+    print("Directory to Process: " + folder_selected)
+    cpt = sum([len(files) for r, d, files in os.walk(folder_selected)])
+    print("Number of Files using listdir method#1 :", cpt)
+
+
+def open_excel(event):
+    os.startfile("results.xlsx")
 
 
 # Define button press function
-def press2():
-    print("start")
-    path = "D:\\OneDrive\\1.Important\\Backups-Pictures\\2015\\03-March\\"
-    files = [f for f in glob.glob(path + "**/*.jpg", recursive=True)]
-    # coord = []
+def process():
+    print("Starting to Parse Image Exif Information")
+    global cpt
+    global status_message
+    status_message.set("")
+    link1.config(text="")
+    link2.config(text="")
+    text.delete('1.0', END)
+    status.config(text="")
+    count = 0
+    path = entryText.get() + "/"
+    # path = entryText.get().replace("/", "//")+"//"
+    files = [f for f in glob.glob(path + "**/*.*", recursive=True)]
     visited_cities = []
     visited_cities_clean = []
+    visited_coordinates_lat = []
+    visited_coordinates_lon = []
+    visited_coordinates = []
+    months = []
+    years = []
+    cities = []
+    countries = []
+
     for f in files:
-        # print(f)
+        f = f.replace("\\", "/")
+        count = count + 1
+        if count % 10 == 0:
+            # print(count)
+            status_message.set("Processing Image: " + str(count) + " of " + str(cpt))
         try:
             exif = get_exif(f)
             geo_tags = get_geotagging(exif)
-            # label.config(text=f"Coordinates: {get_coordinates(geo_tags)}")
-            # coord.append(get_coordinates(geo_tags))
-            results = rg.search(get_coordinates(geo_tags), mode=1)
+            coordinates = get_coordinates(geo_tags)
+            lat = float(Decimal(coordinates[0]).quantize(Decimal(10) ** -3))
+            lon = float(Decimal(coordinates[1]).quantize(Decimal(10) ** -3))
+            results = rg.search(coordinates, mode=1)
             city = results[0].get('name') + ", "
             state = results[0].get('admin1') + ", "
             country = results[0].get('cc')
-            datum = geo_tags['GPSDateStamp']
-            year = str(convert(datum).year)
-            month = str(convert(datum).month)
 
-            visited_cities.append(city + state + country + "|" + year+":"+month)
+            cc = coco.CountryConverter(include_obsolete=True)
+            country = cc.convert(country, to='name_short')
 
-            print("Location:" + city + state + country + " | " + f)
+            try:
+                datum = geo_tags['GPSDateStamp']
+                year = str(converter(datum).year)
+                month = str(converter(datum).month)
+            except:
+                try:
+                    datum = str(Image.open(f)._getexif()[36867]).split(" ", 1)[0]
+                    year = str(converter(datum).year)
+                    month = str(converter(datum).month)
+                except:
+                    year = "1970"
+                    month = "00"
+
+            if len(year) == 1:
+                year = "0" + year
+            if len(month) == 1:
+                month = "0" + month
+
+            # print(f)
+            if year != "1970" and month != "00":
+                visited_cities.append(year + ":" + month + " | " + city + state + country)
+
+            # print("Location:" + city + state + country + " | " + f)
             for word in visited_cities:
                 if word not in visited_cities_clean:
                     visited_cities_clean.append(word)
-            label2.config(text=f"Location: {city}{state}{country}")
+                    label.config(text=f"Processing Coordinates: {coordinates}")
+                    label2.config(text="Processing Location: " + city + state + country)
+                    if lat != 0.000 and lon != 0.000:
+                        visited_coordinates_lat.append(lat)
+                        visited_coordinates_lon.append(lon)
+                        visited_coordinates.append(
+                            city + country + "|" + str(lat) + "|" + str(lon) + "|" + year + ":" + month)
+                        months.append(month)
+                        years.append(year)
+                        cities.append(results[0].get('name'))
+                        countries.append(country)
+                        text.insert(tkinter.END,
+                                    year + ":" + month + " - " + city + country + "\t (" + str(lat) + "," + str(
+                                        lon) + ")\n")
+                        text.see("end")
         except:
             # print("GPS Data Missing in " + f)
             error = 2
-    print("clean:")
-    pprint(visited_cities_clean)
-    print("end")
+
+    status_message.set("Processed: " + str(count) + " images")
+    label.config(text="")
+    label2.config(text="")
+    # print("---------RESULTS----------")
+    # visited_cities.sort()
+    # pprint(visited_cities_clean)
+    # eachInASeparateLine = "\n".join(visited_cities_clean)
+    # text.insert(tkinter.END, eachInASeparateLine)
+    # print("--------------------------")
+
+    print("--- GOOGLE MAP Generated ---")
+    google_map = gmplot.GoogleMapPlotter(0, 0, 3)
+    google_map.coloricon = "http://www.googlemapsmarkers.com/v1/%s/"
+    google_map.apikey = google_api_key
+    google_map.heatmap(visited_coordinates_lat, visited_coordinates_lon)
+    google_map.plot(visited_coordinates_lat, visited_coordinates_lon, c='#046CC6', title="implementation",
+                    label="implementation", edge_width=3.0)
+
+    # ADD MARKERS
+    for word in visited_coordinates:
+        title = word.split("|")[0]
+        lati = word.split("|")[1]
+        long = word.split("|")[2]
+        date = word.split("|")[3]
+        date = date.split(":")
+        month_word = (calendar.month_name[int(date[1])])
+        google_map.marker(float(lati), float(long), 'cornflowerblue',
+                          title=str(title) + " (" + str(date[0]) + " " + str(month_word) + ")")
+
+    google_map.draw("result.html")
+
+    link1.config(text="Open Map")
+    link1.bind("<Button-1>", lambda e: callback("result.html"))
+
+    link2.config(text="Open Excel")
+    link2.bind("<Button-1>", open_excel)
+
+    df = pd.DataFrame(
+        {'Month': months, 'Year': years, 'City': cities, 'Country': countries, 'Lat.': visited_coordinates_lat,
+         'Long.': visited_coordinates_lon})
+    writer = pd.ExcelWriter("results.xlsx", engine='xlsxwriter')
+    df.to_excel(writer, sheet_name='Sheet1', startrow=1, header=False)
+    workbook = writer.book
+    worksheet = writer.sheets['Sheet1']
+
+    header_format = workbook.add_format({
+        'bold': True,
+        'text_wrap': False,
+        'valign': 'top',
+        'fg_color': '#D7E4BC',
+        'border': 1})
+
+    # Write the column headers with the defined format.
+    for col_num, value in enumerate(df.columns.values):
+        worksheet.write(0, col_num + 1, value, header_format)
+
+    # Close the Pandas Excel writer and output the Excel file.
+    df.sort_values(['Month', 'Year'], ascending=[True, True])
+    writer.save()
+
+    text.insert(tkinter.END, "\n")
+    text.insert(tkinter.END, "---------------END---------------\n")
+    text.insert(tkinter.END, "\n")
+    text.see("end")
+    print("-------------END------------")
+
+
+def start_thread():
+    Thread(target=process,
+           daemon=True).start()  # deamon=True is important so that you can close the program correctly
+
+
+def callback(url):
+    webbrowser.open_new(url)
 
 
 # Place 'Change Label' button on the window
-button = tkinter.Button(window, text="Button", command=press2)
-button.place(x=10, y=60)
-button2 = tkinter.Button(window, text="ListFiles", command=press2)
-button2.place(x=10, y=90)
+button = tkinter.Button(window, text="...", command=press)
+button.place(x=470, y=17)
 
+process_button = tkinter.Button(window, text="Process Images", command=start_thread)
+process_button.place(x=10, y=50)
+
+# close the program and tkinter window on exit
+window.protocol("WM_DELETE_WINDOW", ask_quit)
 # Show new window
 window.mainloop()
